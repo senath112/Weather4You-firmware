8e3e4cdf3e3a85bf7a7b68d8150cdd252998bc51
Senath Sethmika <94299314+senath112@users.noreply.github.com>
2022-12-04 07:44:05 +0530

    Update firmware.ino

--- firmware.ino
+++ firmware.ino
@@ -1,7 +1,7 @@
 /*********
   Rui Santos
   Complete instructions at https://RandomNerdTutorials.com/esp32-wi-fi-manager-asyncwebserver/
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 *********/
@@ -11,9 +11,20 @@
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
 #include "SPIFFS.h"
+#include <Firebase_ESP_Client.h>
+#include <Wire.h>
+#include "time.h"
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+#define API_KEY "AIzaSyAl_-mSzachIEJshqgKXqtQY2_6ZhFSv90"
+
+unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = 180000;
+#define DATABASE_URL "https://esp32-weather-app-default-rtdb.europe-west1.firebasedatabase.app/"
 
 // Search for parameter in HTTP POST request
 const char* PARAM_INPUT_1 = "ssid";
@@ -22,6 +33,25 @@ const char* PARAM_INPUT_3 = "ip";
 const char* PARAM_INPUT_4 = "email";
 const char* PARAM_INPUT_5 = "password";
 
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+String uid;
+String databasePath;
+String tempPath = "/temperature";
+String humPath = "/humidity";
+String presPath = "/pressure";
+String timePath = "/timestamp";
+
+String parentPath;
+
+int timestamp;
+FirebaseJson json;
+const char* ntpServer = "pool.ntp.org";
+
+float temperature;
+float humidity;
+float pressure;
 
 //Variables to save values from HTML form
 String ssid;
@@ -64,33 +94,33 @@ void initSPIFFS() {
 }
 
 // Read File from SPIFFS
-String readFile(fs::FS &fs, const char * path){
+String readFile(fs::FS &fs, const char * path) {
   Serial.printf("Reading file: %s\r\n", path);
 
   File file = fs.open(path);
-  if(!file || file.isDirectory()){
+  if (!file || file.isDirectory()) {
     Serial.println("- failed to open file for reading");
     return String();
   }
-  
+
   String fileContent;
-  while(file.available()){
+  while (file.available()) {
     fileContent = file.readStringUntil('\n');
-    break;     
+    break;
   }
   return fileContent;
 }
 
 // Write file to SPIFFS
-void writeFile(fs::FS &fs, const char * path, const char * message){
+void writeFile(fs::FS &fs, const char * path, const char * message) {
   Serial.printf("Writing file: %s\r\n", path);
 
   File file = fs.open(path, FILE_WRITE);
-  if(!file){
+  if (!file) {
     Serial.println("- failed to open file for writing");
     return;
   }
-  if(file.print(message)){
+  if (file.print(message)) {
     Serial.println("- file written");
   } else {
     Serial.println("- write failed");
@@ -99,7 +129,7 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
 
 // Initialize WiFi
 bool initWiFi() {
-  if(ssid=="" || ip==""){
+  if (ssid == "" || ip == "") {
     Serial.println("Undefined SSID or IP address.");
     return false;
   }
@@ -109,7 +139,7 @@ bool initWiFi() {
   localGateway.fromString("192, 168, 1, 1");
 
 
-  if (!WiFi.config(localIP, localGateway, subnet)){
+  if (!WiFi.config(localIP, localGateway, subnet)) {
     Serial.println("STA Failed to configure");
     return false;
   }
@@ -119,7 +149,7 @@ bool initWiFi() {
   unsigned long currentMillis = millis();
   previousMillis = currentMillis;
 
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     currentMillis = millis();
     if (currentMillis - previousMillis >= interval) {
       Serial.println("Failed to connect.");
@@ -133,8 +163,8 @@ bool initWiFi() {
 
 // Replaces placeholder with LED state value
 String processor(const String& var) {
-  if(var == "STATE") {
-    if(digitalRead(ledPin)) {
+  if (var == "STATE") {
+    if (digitalRead(ledPin)) {
       ledState = "ON";
     }
     else {
@@ -144,17 +174,28 @@ String processor(const String& var) {
   }
   return String();
 }
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return (0);
+  }
+  time(&now);
+  return now;
+}
 
 void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
-
+  configTime(0, 0, ntpServer);
+  config.api_key = API_KEY;
   initSPIFFS();
 
   // Set GPIO 2 as an OUTPUT
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  
+
   // Load values saved in SPIFFS
   ssid = readFile(SPIFFS, ssidPath);
   pass = readFile(SPIFFS, passPath);
@@ -166,26 +207,20 @@ void setup() {
   Serial.println(ip);
   Serial.println(email);
   Serial.println(password);
+  auth.user.email = email;
+  auth.user.password = password;
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+  config.token_status_callback = tokenStatusCallback;
+  config.max_token_generation_retry = 5;
+  Firebase.begin(&config, &auth);
+  databasePath = "/UsersData/" + uid + "/readings";
 
-  if(initWiFi()) {
+
+  if (initWiFi()) {
     // Route for root / web page
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-      request->send(SPIFFS, "/index.html", "text/html", false, processor);
-    });
-    server.serveStatic("/", SPIFFS, "/");
-    
-    // Route to set GPIO state to HIGH
-    server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) {
-      digitalWrite(ledPin, HIGH);
-      request->send(SPIFFS, "/index.html", "text/html", false, processor);
-    });
-
-    // Route to set GPIO state to LOW
-    server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) {
-      digitalWrite(ledPin, LOW);
-      request->send(SPIFFS, "/index.html", "text/html", false, processor);
-    });
-    server.begin();
+    WiFi.disconnect();
+    WiFi.begin(ssid.c_str(), pass.c_str());
   }
   else {
     // Connect to Wi-Fi network with SSID and password
@@ -195,20 +230,20 @@ void setup() {
 
     IPAddress IP = WiFi.softAPIP();
     Serial.print("AP IP address: ");
-    Serial.println(IP); 
+    Serial.println(IP);
 
     // Web Server Root URL
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
       request->send(SPIFFS, "/wifimanager.html", "text/html");
     });
-    
+
     server.serveStatic("/", SPIFFS, "/");
-    
-    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+
+    server.on("/", HTTP_POST, [](AsyncWebServerRequest * request) {
       int params = request->params();
-      for(int i=0;i<params;i++){
+      for (int i = 0; i < params; i++) {
         AsyncWebParameter* p = request->getParam(i);
-        if(p->isPost()){
+        if (p->isPost()) {
           // HTTP POST ssid value
           if (p->name() == PARAM_INPUT_1) {
             ssid = p->value().c_str();
@@ -253,6 +288,8 @@ void setup() {
       }
       request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
       delay(3000);
+      //*********************************************
+
       ESP.restart();
     });
     server.begin();
@@ -260,7 +297,36 @@ void setup() {
 }
 
 void loop() {
-Serial.println(ssid);
+  if (ssid == "" || ip == "") {
+  Serial.println("Undefined");
+  }else{
+    Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)) {
+    sendDataPrevMillis = millis();
+
+    //Get current timestamp
+    timestamp = getTime();
+    Serial.print ("time: ");
+    Serial.println (timestamp);
+
+    parentPath = databasePath + "/" + String(timestamp);
+
+    json.set(tempPath.c_str(), String(35));
+    json.set(humPath.c_str(), String(55));
+    json.set(presPath.c_str(), String(104056 / 100.0F));
+    json.set(timePath, String(timestamp));
+    Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+  }
+  }
+  
+  Serial.println(ssid);
   Serial.println(pass);
   Serial.println(ip);
   Serial.println(email);
